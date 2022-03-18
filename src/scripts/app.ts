889a38f26777a714ecebc509ff82f7d3a13d2c1b
Suff <wladfrommatrix@gmail.com>
2022-03-18 20:00:37 +0400

    Add changing of active-btn class on click

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,4 +1,16 @@
 const section = document.querySelectorAll(".section");
-const sectionBtns = document.querySelectorAll(".controls");
-const sectionBtn = document.querySelectorAll(".control");
+const controlWrapper = document.querySelectorAll(".controls");
+const controlBtns = document.querySelectorAll(".control");
 const allSections = document.querySelectorAll(".main-content");
+
+let PageTransition = () => {
+  controlBtns.forEach((c: Element) => {
+        c.addEventListener('click', (e: Event) => {
+            let currentBtn = document.querySelectorAll('.active-btn')
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '')
+            let clickedBtn = <Element>e.target;
+            clickedBtn.className += ' active-btn'
+        })
+  });
+};
+PageTransition();
